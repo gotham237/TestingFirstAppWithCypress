69f5f17c86a76f9645825b81a5ad471bffb6580b
Szym_tym <gotham237@gmail.com>
2023-07-13 20:52:58 +0200

    added new user journey

--- cypress/e2e/user-journey.cy.js
+++ cypress/e2e/user-journey.cy.js
@@ -1,6 +1,9 @@
 describe("User Journey", () => {
-  it("a user can find a course on the home page and complete the courses lessons", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:3000")
+  })
+
+  it("a user can find a course on the home page and complete the courses lessons", () => {
     cy.getByData("course-0").find("a").contains("Get started").click()
     cy.location("pathname").should("equal", "/testing-your-first-application")
     cy.getByData("next-lesson-button").click()
@@ -23,5 +26,18 @@ describe("User Journey", () => {
     cy.getByData("challenge-answer-0").click()
     cy.getByData("next-lesson-button").click()
     cy.location("pathname").should("eq", "/")
+  })
+
+  it("User subscribes to the newsletter and finishes app-install-and-overview 1 part", () => {
+    cy.get('[data-test="email-input"]').type("gotham@test.com")
+    cy.get('[data-test="submit-button"]').click()
+    cy.get('[data-test="success-message"]')
+      .should("exist")
+      .contains("Success: gotham@test.com has been successfully subscribed")
+    cy.get('[data-test="course-0"]').find("a").contains("Get started").click()
+    cy.location("pathname").should("eq", "/testing-your-first-application")
+    cy.get('[data-test="next-lesson-button"]').click()
+    cy.location("pathname").should("eq", "/testing-your-first-application/app-install-and-overview")
+
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,7 @@
+beforeEach(() => {
+  cy.log("I run before every spec file")
+})
+
 Cypress.Commands.add("getByData", (selector) => {
   return cy.get(`[data-test=${selector}]`)
 })
